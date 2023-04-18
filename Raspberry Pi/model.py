dbda327134713a9a073ebb950616898fa2f7493b
Leon Kabue <kabue@seas.upenn.edu>
2023-04-18 04:51:11 -0400

    model

--- Raspberry Pi/model.py
+++ Raspberry Pi/model.py
@@ -5,22 +5,25 @@ import time
 
 
 class Model:
-    def __init__(self, interpreter=Interpreter("model.tflite")):
-        self.interpreter.allocate_tensors()
+    interpreter=None
+    def __init__(self):
+        global interpreter
+        interpreter.allocate_tensors()
         
     def classifyImage(self, img):
-        input_index = self.interpreter.get_input_details()[0]['index']
-        output_index = self.interpreter.get_output_details()[0]['index']
+        global interpreter
+        input_index = interpreter.get_input_details()[0]['index']
+        output_index = interpreter.get_output_details()[0]['index']
 
         img = img.resize((100, 100))
         img_array = np.asarray(img)
         input_tensor = np.expand_dims(img_array, 0)
     
-        self.interpreter.set_tensor(input_index, input_tensor)
+        interpreter.set_tensor(input_index, input_tensor)
 
-        self.interpreter.invoke()
+        interpreter.invoke()
 
-        prediction =  self.interpreter.get_tensor(output_index)[0]
+        prediction =  interpreter.get_tensor(output_index)[0]
 
         return prediction
 
