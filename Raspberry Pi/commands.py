a6524c4df2902a695fb77a3ea3c15965c95c7963
kenzoSakamoto <36580624+kenzoSakamoto@users.noreply.github.com>
2023-05-09 17:46:00 -0400

    Upload raspberry pi demo files

--- Raspberry Pi/commands.py
+++ Raspberry Pi/commands.py
@@ -5,9 +5,10 @@ MAX_SPEED = 200
 VALID_MOVE = ['F', 'B', 'R', 'L', 'U', 'S']
 
 class Commander:
-    def __init__(self, baud=9600, port='/dev/ttyS0'):
+    def __init__(self, baud=9600, port='/dev/ttyACM0'):
         self.ser = serial.Serial(port=port, baudrate=baud, timeout=0.1, stopbits=serial.STOPBITS_TWO)
         self.ser.flush()
+        self.ser.flushInput()
 
     def setSpeed(self, leftSpeed, rightSpeed, dur):
         leftSpeed = inRange(int(leftSpeed), MIN_SPEED, MAX_SPEED)
@@ -15,7 +16,7 @@ class Commander:
         dur = int(dur)
     
         self.ser.write(f"SPEED {leftSpeed} {rightSpeed} {dur}\n".encode('ascii'))
-        return self.ser.readline().decode('utf-8')
+        return 0
     
     def move(self, dir):
         if dir not in VALID_MOVE:
@@ -23,7 +24,50 @@ class Commander:
             return -1
         
         self.ser.write(f"MOVE {dir}\n".encode('ascii'))
-        return self.ser.readline().decode('utf-8')
+        return 0
+    
+    def ron(self):
+        self.ser.write(f"RON\n".encode('ascii'))
+        return 0
+    
+    def roff(self):
+        self.ser.write(f"ROFF\n".encode('ascii'))
+        return 0
+    
+    def gon(self):
+        self.ser.write(f"GON\n".encode('ascii'))
+        return 0
+    
+    def goff(self):
+        self.ser.write(f"GOFF\n".encode('ascii'))
+        return 0
+    
+    def bon(self):
+        self.ser.write(f"BON\n".encode('ascii'))
+        return 0
+    
+    def boff(self):
+        self.ser.write(f"BOFF\n".encode('ascii'))
+        return 0
+    
+    def flush(self):
+        self.ser.flush()
+        self.ser.flushInput()
+
+        return 0
+    
+    def getDistF(self):        
+        self.ser.write(f"DISTF\n".encode('ascii'))
+        line = self.ser.readline().decode('utf-8')
+        
+        try:
+            dist = int(line)
+        except: 
+            print("Failed to cast dist to int")
+            print(f"Got line: {line}")
+            return -1
+        return dist
+
     
     def getDistC(self):        
         self.ser.write(f"DISTC\n".encode('ascii'))
@@ -36,22 +80,24 @@ class Commander:
             print(f"Got line: {line}")
             return -1
         return dist
+
     
-    def getDistF(self):        
-        self.ser.write(f"DIST\n".encode('ascii'))
+    def test(self):        
+        self.ser.write(f"TEST\n".encode('ascii'))
         line = self.ser.readline().decode('utf-8')
         
         try:
-            dist = int(line)
+            dist = str(line)
         except: 
             print("Failed to cast dist to int")
             print(f"Got line: {line}")
             return -1
         return dist
+
     
     def noPerson(self):        
         self.ser.write(f"NOPERSON\n".encode('ascii'))
-        return self.ser.readline().decode('utf-8')
+        return 0
     
     def person(self, angle):
         try:
@@ -63,7 +109,7 @@ class Commander:
 
         
         self.ser.write(f"PERSON {int(angle)}\n".encode('ascii'))
-        return self.ser.readline().decode('utf-8')
+        return 0
     
     def waitUntilDone(self):
         line = self.ser.readline().decode('utf-8')
--- Raspberry Pi/sshController.py
+++ Raspberry Pi/sshController.py
@@ -8,8 +8,10 @@ comm = Commander()
 
 speed_matcher = re.compile("SPEED (\d+) (\d+) (\d+)")
 move_matcher = re.compile("MOVE ([FBRLUS])")
-dist_matcher = re.compile("DIST")
+distf_matcher = re.compile("DISTF")
+distc_matcher = re.compile("DISTC")
 noperson_matcher = re.compile("NOPERSON")
+test_matcher = re.compile("TEST")
 person_matcher = re.compile("PERSON (\d+)")
 
 while True:
@@ -17,58 +19,69 @@ while True:
 
     if command.lower() == 's':
         print(f"Got: STOP")
-        print(comm.move('S'))
+        comm.move('S')
         continue
 
     if command.lower() == 'f':
         print(f"Got: F")
-        print(comm.move('B'))
+        comm.move('F')
         continue
 
     if command.lower() == 'b':
         print(f"Got: B")
-        print(comm.move('F'))
+        comm.move('B')
         continue
 
     if command.lower() == 'r':
         print(f"Got: R")
-        print(comm.move('R'))
+        comm.move('R')
         continue
 
 
-    if command.lower() == 'L':
+    if command.lower() == 'l':
         print(f"Got: L")
-        print(comm.move('L'))
+        comm.move('L')
         continue
 
 
     if command.lower() == 'u':
         print(f"Got: U")
-        print(comm.move('U'))
+        comm.move('U')
         continue
 
     # SPEED
     matched = speed_matcher.match(command)
     if (matched):
         print(f"Got: SPEED {matched.group(1)} {matched.group(2)} {matched.group(3)}")
-        print(comm.setSpeed(matched.group(1), matched.group(2), matched.group(3)))
+        comm.setSpeed(matched.group(1), matched.group(2), matched.group(3))
         continue
 
     # MOVE
     matched = move_matcher.match(command)
     if (matched):
         print(f"Got: MOVE {matched.group(1)}")
-        print(comm.move(matched.group(1)))
+        comm.move(matched.group(1))
         continue
 
     # DIST
-    matched = dist_matcher.match(command)
+    matched = distf_matcher.match(command)
     if (matched):
-        print(f"Got: DIST")
-        dist = comm.getDist()
+        print(f"Got: DISTF")
+        dist = comm.getDistF()
         if (dist > 0):
             print(f"Distance read: {dist}")
         continue
+
+
+    # DIST
+    matched = distc_matcher.match(command)
+    if (matched):
+        print(f"Got: DISTC")
+        dist = comm.getDistC()
+        if (dist > 0):
+            print(f"Distance read: {dist}")
+        continue
+
 
     # NO PERSON
     matched = noperson_matcher.match(command)
@@ -83,6 +96,71 @@ while True:
         print(f"Got: PERSON {matched.group(1)}")
         comm.person(matched.group(1))
         continue  
+
+    # TEST
+    matched = test_matcher.match(command)
+    if (matched):
+        print(f"Got: TEST")
+        print('Received ' + comm.test())
+        continue
+
+    matched = command.upper() == 'RON'
+    if (matched):
+        print(f"Got: RON")
+        comm.ron()
+        continue
+
+    matched = command.upper() == 'ROFF'
+    if (matched):
+        print(f"Got: ROFF")
+        comm.roff()
+        continue
+
+    matched = command.upper() == 'GON'
+    if (matched):
+        print(f"Got: GON")
+        comm.gon()
+        continue
+
+    matched = command.upper() == 'GOFF'
+    if (matched):
+        print(f"Got: GOFF")
+        comm.goff()
+        continue
+
+    matched = command.upper() == 'BON'
+    if (matched):
+        print(f"Got: BON")
+        comm.bon()
+        continue
+
+    matched = command.upper() == 'BOFF'
+    if (matched):
+        print(f"Got: BOFF")
+        comm.boff()
+        continue
+
+    matched = command.upper() == 'ON'
+    if (matched):
+        print(f"Got: ON")
+        comm.ron()
+        comm.gon()
+        comm.bon()
+        continue
+
+    matched = command.upper() == 'OFF'
+    if (matched):
+        print(f"Got: OFF")
+        comm.roff()
+        comm.goff()
+        comm.boff()
+        continue
+
+    matched = command.upper() == 'FLUSH'
+    if (matched):
+        print(f"Got: FLUSH")
+        comm.flush()
+        continue
 
     else:
         print("Command not recognized, stopping")
